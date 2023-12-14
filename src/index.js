1c2939664da4b4196591908696b20408c9e5c19e
pompokorin <147459878+pompokorin@users.noreply.github.com>
2023-12-14 20:07:49 +1100

    update js
    added typewriter feature when displaying the poem

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "The candle is burning",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemElement = document.querySelector("#keyword");
+poemElement.addEventListener("submit", generatePoem);
