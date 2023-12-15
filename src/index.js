897bd439e4a8b563d03bcd20e6cd287301c6d27d
pompokorin <147459878+pompokorin@users.noreply.github.com>
2023-12-15 21:09:58 +1100

    update js - AI API added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "The candle is burning",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
-let poemElement = document.querySelector("#keyword");
-poemElement.addEventListener("submit", generatePoem);
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionElement = document.querySelector("#user-instruction");
+  let apiKey = "040ffb19o36e1562a0f417abf724b2t9";
+  let context =
+    "You are a Haiku master. Your mission is to generate a haiku in basic html and seperate each line in <br/>. Make sure to follow the user instructions . Do not include a title and only display the haiku. Leave one line after Haiku and sign the poem with 'SheCodes Ai' inside a <small> element at the end and NOT at first";
+  let prompt = `User instructions: Generate a Haiku about ${instructionElement.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+  console.log(prompt);
+}
+
+let poemFormElement = document.querySelector("#keyword");
+poemFormElement.addEventListener("submit", generatePoem);
